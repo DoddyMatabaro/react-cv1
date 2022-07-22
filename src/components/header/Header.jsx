0600d7b804020f528af1cb75ab20ba5e3843e919
Doddy MATABARO <matabarododdy@gmail.com>
2022-07-22 14:36:01 +0200

    feat: header components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import CTA from './CTA'
 import './header.css'
-import ME from '../../assets/profile.jpg'
+import ME from '../../assets/profile.png'
 import HeaderSocials from './HeaderSocials'
+import Typewriter from 'typewriter-effect';
 
 const Header = () => {
+
   return (
    <header >
-        <div className="container header_container">
-            <h5> Hello I'm </h5>
-            <h1 > Doddy MATABARO</h1>
-            <h5 className='text-light'>Fullstack Developper</h5>
+        <div className="container header__container">
+        <Typewriter
+              onInit={(typewriter) => {
+                typewriter.typeString('<h5>Hello I\'m</h5>')
+                .pauseFor(3000)
+                 typewriter.typeString('<h1>Doddy MATABARO</h1>')                
+                  .pauseFor(3500)
+                 typewriter.typeString('<h5 class="text-light">Fullstack Developper</h5>')                
+                  .deleteAll()
+                  .start();
+              }}
+/>
             <CTA />
-            <HeaderSocials />
             <div className="me">
                   <img src={ME} alt=""/>
             </div>
-      
+            <HeaderSocials />
+   
             <a href='#contact' className='scroll__down'>Scroll Down</a>
         </div>
    </header>
