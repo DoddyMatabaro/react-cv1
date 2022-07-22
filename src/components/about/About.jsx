48e9c25256bb26ef82ec1e9d464447357bc171a7
Doddy MATABARO <matabarododdy@gmail.com>
2022-07-22 14:39:59 +0200

    feat: add about componnents and his elements

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/profile.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
+
   return (
-    <div>About</div>
+    <section id='about'>
+        <h5>Get to know</h5>
+        <h2>About Me</h2>
+        <div className='container about__container'>
+          <div className='about__me'>
+              <div className='about__me-img'>
+                <img src={ME} alt="About image" />
+              </div>
+          </div>
+          <div className='about__content'>
+              <div className='about__cards'>
+                  <article className='about__card'>
+                    <FaAward className='about__icon'/>
+                    <h5>Experience</h5>
+                    <small>3+ Years working</small>
+                  </article>
+                  <article className='about__card'>
+                    <FiUsers className='about__icon'/>
+                    <h5>Clients</h5>
+                    <small>200 Worldwide</small>
+                  </article>
+                  <article className='about__card'>
+                    <VscFolderLibrary className='about__icon'/>
+                    <h5>Projets</h5>
+                    <small>200 Worldwide</small>
+                  </article>
+              </div>
+              <p>idjskldjlskjlksjjklhajkhfjkdshfjklshadfjkasdhfjklashdfjklhsdjklfhkljsjdfkldshfjsafjsdhfklsdfcjshdskhf
+                lsjkdhflsfhdljkhfdjkhfjksdhfk</p>
+          </div>
+        </div>
+    </section>
   )
 }
 
